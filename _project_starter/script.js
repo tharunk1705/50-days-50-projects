2ca6d415159b1c40d82ea6131400fa86d40d144b
Tharun Kumar <tharunk1705@gmail.com>
2020-12-04 23:13:58 +0530

    Completed First Project - Expanding Cards

--- _project_starter/script.js
+++ _project_starter/script.js
@@ -4,11 +4,11 @@ panels.forEach((panel) => {
     panel.addEventListener('click', () => {
         removeActiveClasses();
         panel.classList.add('active');
-    })
-})
+    });
+});
 
 const removeActiveClasses = () => {
     panels.forEach(panel => {
         panel.classList.remove('active');
-    })
+    });
 }
