c67519eb5855c80d238608a7bfd8cd5df191bba9
Tharun Kumar <tharunk1705@gmail.com>
2020-12-05 20:23:15 +0530

    Third project - Circle Nav Styling

---
+++ 03.Circle Styling/script.js
@@ -0,0 +1,13 @@
+const open  = document.getElementById('open');
+const close =  document.getElementById('close');
+const container = document.querySelector('.container');
+
+console.log(open);
+
+open.addEventListener('click', ()=>{
+    container.classList.add('show-nav');
+});
+
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav');
+})
